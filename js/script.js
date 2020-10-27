332307a4dc994b34c285b8665903a7881f626fcc
Melina Nadal <melinanhopkins@gmail.com>
2020-10-27 12:02:36 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://coinranking1.p.rapidapi.com/coins",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "coinranking1.p.rapidapi.com",
+		"x-rapidapi-key": "c0f227240amsh55aa92563c9e45cp1e4b12jsn976987091817"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
