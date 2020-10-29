7fe1c9b1e584a81181e3221989739f162ef99503
Melina Nadal <melinanhopkins@gmail.com>
2020-10-29 00:18:03 -0400

    API connect

---
+++ README.md
@@ -0,0 +1,3 @@
+CryptoData Finder
+
+The purpose of the CryptoData Finder is to get information on a crypto currencey of your choosing in real time.
--- js/script.js
+++ js/script.js
@@ -1,15 +1,51 @@
-console.log('working');
-// var settings = {
-// 	"async": true,
-// 	"crossDomain": true,
-// 	"url": "https://coinranking1.p.rapidapi.com/coins",
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "coinranking1.p.rapidapi.com",
-// 		"x-rapidapi-key": "c0f227240amsh55aa92563c9e45cp1e4b12jsn976987091817"
-// 	}
-// }
 
-// $.ajax(settings).done(function (response) {
-// 	console.log(response);
-// // });
+
+let cryptoData = { }
+let userInput;
+
+const $name = $('#name');
+const $symbol = $('#symbol');
+const $price = $('#price');
+const $description = $('#description');
+
+
+$('form').on('submit', handleGetData);
+
+function handleGetData(event) {
+	event.preventDefault();
+	$userInput= $('#input-box');
+  userInput = $userInput.val();
+  console.log(userInput)
+  
+
+  
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": `https://coinranking1.p.rapidapi.com/coin/${userInput}`,
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+      "x-rapidapi-key": "c0f227240amsh55aa92563c9e45cp1e4b12jsn976987091817"
+    }
+  }
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    cryptoData.name=response.data.coin.name
+    cryptoData.symbol=response.data.coin.symbol
+    cryptoData.price=response.data.coin.price
+    cryptoData.description=response.data.coin.description
+    render()
+  });
+}
+
+
+ function render() {
+  $name.text(cryptoData.name);
+  $symbol.text(cryptoData.symbol);
+	$price.text(cryptoData.price);
+	$description.text(cryptoData.description);
+ }
+  
+	
